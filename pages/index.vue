30c24911e0a69a0809a57bbd870eac5f9d0d43a1
thangeswariy <50661175+thangeswariy@users.noreply.github.com>
2020-07-22 21:16:16 +0800

    Updated with my resume content

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,85 @@
 <template>
   <div class="flex flex-col w-screen h-full">
     <!-- Your name, profile picture and contact -->
-    <HeadWrapper 
-      name='Your Name'
-      title='Your Job Title'
-      userImgURL='Image URL Address'
-      phone='Your Contact Number'
-      email='Your Email Contact'
+    <HeadWrapper
+      name="Thangeswariy"
+      title="Full Stack Developer"
+      userImgURL="../img/Thangeswariy.png"
+      phone="012-4961629"
+      email="thangeswariyramanei@gmail.com"
     />
     <!-- About Me -->
-    <AboutWrapper 
-      aboutMe='Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.'
-      linkedInURL='Your LinkedIn URL Address'
-      languages='Languages'
-      nationality='Your Nationality'
-      interest='Your Interest'
+    <AboutWrapper
+      aboutMe="Solution creator|Data Scientist."
+      linkedInURL="https://www.linkedin.com/in/thangeswariyramanei/"
+      languages="English, Malay, Tamil"
+      nationality="Malaysian"
+      interest="Classical music string instrument"
     />
     <!-- Working Experience -->
     <CareerWrapper>
       <template v-slot:career>
-        <Career 
-          companyName='Company Name'
-          title='Your Job Title'
-          startDate='Start Date'
-          endDate='End Date'
-          >
+        <Career
+          companyName="Sony EMCS PG Tec Sdn Bhd"
+          title="Senior Executive"
+          startDate="1st Apr 2019"
+          endDate="30th June 2020"
+        >
           <!-- List of what you had did in the company -->
           <template v-slot:experience>
-            <ul class='text-gray-600 list-inside list-disc mt-2 px-4'>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
+            <ul class="text-gray-600 list-inside list-disc mt-2 px-4">
+              <li>Order Management.</li>
+              <li>Effective communication with logistics and production planners.</li>
+              <li>Implementation of cumulative approach to minimize excess parts.</li>
             </ul>
           </template>
         </Career>
-        
-        <Career 
-          companyName='Company Name'
-          title='Your Job Title'
-          startDate='Start Date'
-          endDate='End Date'
-          >
+
+        <Career
+          companyName="Sony EMCS PG TEC Sdn Bhd"
+          title="Executive"
+          startDate="19th December 2016"
+          endDate="31st March 2019"
+        >
           <!-- List of what you had did in the company -->
           <template v-slot:experience>
-            <ul class='text-gray-600 list-inside list-disc mt-2 px-4'>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
+            <ul class="text-gray-600 list-inside list-disc mt-2 px-4">
+              <li>97% standard DS achieved.</li>
+              <li>advance shipment</li>
+              <li>data integration for P & L report.</li>
             </ul>
           </template>
         </Career>
       </template>
     </CareerWrapper>
     <!-- Achievement -->
-    <AchievementWrapper 
-      project='Number of Project'
-      client='Number of Happy Clients'
-      award='Number of Awards'
-    />
+    <AchievementWrapper project="6" client="6" award="6"/>
     <!-- Portfolio -->
     <PortfolioWrapper>
-        <template v-slot:portfolio>
-          <div class='grid grid-cols-1 md:grid-cols-3'>
-            <ProjectCard 
-              imgUrl='../img/github.png'
-              name='Project Name'
-              project='Project Type'
-              projectURL='https://github.com'
-              />
+      <template v-slot:portfolio>
+        <div class="grid grid-cols-1 md:grid-cols-3">
+          <ProjectCard
+            imgUrl="../img/thangeswariy.png"
+            name="My portfolio"
+            project="Project Mission"
+            projectURL="https://github.com"
+          />
 
-              <ProjectCard 
-              imgUrl='../img/github.png'
-              name='Project Name'
-              project='Project Type'
-              projectURL='https://github.com'
-              />
-              
-              <ProjectCard 
-              imgUrl='../img/github.png'
-              name='Project Name'
-              project='Project Type'
-              projectURL='https://github.com'
-              />
-          </div>
-        </template>
+          <ProjectCard
+            imgUrl="../img/github.png"
+            name="Project Name"
+            project="Project Type"
+            projectURL="https://github.com"
+          />
+
+          <ProjectCard
+            imgUrl="../img/github.png"
+            name="Project Name"
+            project="Project Type"
+            projectURL="https://github.com"
+          />
+        </div>
+      </template>
     </PortfolioWrapper>
   </div>
 </template>
@@ -92,8 +88,8 @@
 export default {
   head() {
     return {
-      title: 'Your Name'
-    }
+      title: "Thangeswariy"
+    };
   }
-}
+};
 </script>
